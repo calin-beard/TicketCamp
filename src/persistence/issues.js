9781a54fd96cf5843ea7a53f16166be1b44914eb
Kevin <claw.ideas@gmail.com>
2017-11-02 20:40:32 +0200

    Additions and updates to issues and projects API.

--- src/persistence/issues.js
+++ src/persistence/issues.js
@@ -23,8 +23,7 @@ const issuesTable = "issues"; //todo change if necessary
 /** CREATE equivalent
  * @param item The object to be added
  */
-export function createIssue(item) {
-    add(item, issuesTable);
+export function createIssue(title, type, description, assignee, childOf, blockerOf) {
 
     // b = b || 0; (e.g. 0 is default)
 
@@ -35,7 +34,51 @@ export function createIssue(item) {
     // }
 }
 
-function addDescription() {
+/** UPDATE title
+ * @param old Old value
+ * @param _new New value
+ */
+function editTitle(old, _new) {
+    //
+}
+
+/** UPDATE type
+ * @param old Old value
+ * @param _new New value
+ */
+function editType(old, _new) {
+    //
+}
+
+/** UPDATE description
+ * @param old Old value
+ * @param _new New value
+ */
+function editDescription(old, _new) {
+    //
+}
+
+/** UPDATE assignee
+ * @param old Old value
+ * @param _new New value
+ */
+function editAssignee(old, _new) {
+    //
+}
+
+/** UPDATE parent value
+ * @param old Old value
+ * @param _new New value
+ */
+function editChildOf(old, _new) {
+    //
+}
+
+/** UPDATE the issue currently blocked by this
+ * @param old Old value
+ * @param _new New value
+ */
+function editBlockerOf(old, _new) {
     //
 }
 
@@ -44,13 +87,6 @@ function addDescription() {
  */
 export function getIssue(item) {
     get(item, issuesTable);
-}
-
-/** UPDATE equivalent
- * @param item The object to be updated
- */
-export function editIssue(item) {
-    update(item, issuesTable);
 }
 
 /** DELETE equivalent
--- src/persistence/projects.js
+++ src/persistence/projects.js
@@ -6,11 +6,11 @@ import {add, get, update, remove} from 'dbHandler';
  * @name Projects table
  * @field id (auto)
  * @field title (required)
+ * @field owner (required)
  * @field description (optional)
  * @field company (optional)
  * @field creationDate (auto)
  * @field lastUpdatedDate (auto)
- * @field owner (required)
  * @field members (optional)
  * @field history (auto)
  */
@@ -20,8 +20,49 @@ const projectsTable = "projects"; //todo change if necessary
 /** CREATE equivalent
  * @param item The object to be added
  */
-export function createProject(item) {
-    add(item, projectsTable);
+export function createProject(title, owner, description, company, members) {
+
+    // add(item, projectsTable);
+}
+
+/** UPDATE title
+ * @param old Old value
+ * @param _new New value
+ */
+function editTitle(old, _new) {
+    //
+}
+
+/** UPDATE owner
+ * @param old Old value
+ * @param _new New value
+ */
+function editOwner(old, _new) {
+    //
+}
+
+/** UPDATE description
+ * @param old Old value
+ * @param _new New value
+ */
+function editDescription(old, _new) {
+    //
+}
+
+/** UPDATE company
+ * @param old Old value
+ * @param _new New value
+ */
+function editCompany(old, _new) {
+    //
+}
+
+/** UPDATE members
+ * @param old Old value
+ * @param _new New value
+ */
+function editMembers(old, _new) {
+    //
 }
 
 /** READ equivalent
@@ -29,13 +70,6 @@ export function createProject(item) {
  */
 export function getProject(item) {
     get(item, projectsTable);
-}
-
-/** UPDATE equivalent
- * @param item The object to be updated
- */
-export function editProject(item) {
-    update(item, projectsTable);
 }
 
 /** DELETE equivalent
