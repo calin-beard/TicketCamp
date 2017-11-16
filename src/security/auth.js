a1941479009acb5a94e1efa1b89da40d1fa41d45
Kevin <claw.ideas@gmail.com>
2017-11-17 00:05:17 +0200

    Authentication / authorization sample code.

--- src/security/auth.js
+++ src/security/auth.js
@@ -1,17 +1,84 @@
 'use strict';
 
 exports.ACTIONS = {
-    READ_ISSUE :        {value: 0, name: "Read issue information"},
-    WRITE_ISSUE :       {value: 1, name: "Modify issue information"},
-    READ_PROJECT_INFO : {value: 2, name: "Read project information"}
+    "READ_ISSUE" : readIssue,
+    "MODIFY_ISSUE" : modifyIssue,
+    "CREATE_ISSUE" : createIssue,
+    "READ_PROJECT_INFO" : readProjectInfo,
+    "MODIFY_PROJECT_INFO" : modifyProjectInfo
 };
 
 exports.isAuthenticated = function(user) {
-
-    //todo
+    // todo
 };
 
 exports.isAuthorized = function(user, action) {
 
-    //todo
+    // todo
+    let permissions = readPermissions(user);
+    if(permissions[action]) {
+        // perform action
+    } else {
+        // reject action
+    }
 };
+
+function readPermissions(userId) {
+    // todo
+}
+
+function readIssue() {
+    // todo
+}
+
+function modifyIssue() {
+    // todo
+}
+
+function createIssue() {
+    // todo
+}
+
+function readProjectInfo() {
+    // todo
+}
+
+function modifyProjectInfo() {
+    // todo
+}
+
+// var tmpFunc = new Function(codeToRun);
+// tmpFunc();
+
+// var jsonFun = {
+//     fun1 : function(){
+//         alert('Done');
+//     }
+// };
+//
+// $('button').click(function(){
+//     jsonFun.fun1();
+// });
+
+// function doSomething (condition) {
+//     var stuff = {
+//         'one': function () {
+//             return 'one';
+//         },
+//
+//         'two': function () {
+//             return 'two';
+//         },
+//
+//         'three': function () {
+//             return 'three';
+//         }
+//     };
+//
+//     if (typeof stuff[condition] !== 'function') {
+//         return 'default';
+//     }
+//
+//
+//     return stuff[condition]();
+// }
